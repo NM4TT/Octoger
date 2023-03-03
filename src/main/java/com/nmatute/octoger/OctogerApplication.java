a5e50daa1d76502dd5357f05c0b4e1400b28347d
NM4TT <manuelnmatute@gmail.com>
2023-03-02 19:58:31 -0500

    first commit

---
+++ src/main/java/com/nmatute/octoger/OctogerApplication.java
@@ -0,0 +1,13 @@
+package com.nmatute.octoger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctogerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctogerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmatute/octoger/OctogerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmatute.octoger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctogerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
