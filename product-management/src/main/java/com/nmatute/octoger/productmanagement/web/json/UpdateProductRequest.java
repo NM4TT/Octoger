de1e5d136f158a96ff1dd6a122dadfcf43f32d91
NM4TT <manuelnmatute@gmail.com>
2023-04-26 16:15:17 -0500

    cambios minimos por imports sin uso

--- product-management/src/main/java/com/nmatute/octoger/productmanagement/web/json/UpdateProductRequest.java
+++ product-management/src/main/java/com/nmatute/octoger/productmanagement/web/json/UpdateProductRequest.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
